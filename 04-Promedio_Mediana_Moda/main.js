da8928c0bb1e652f28832ba2e0b6a78585fe562d
Harold Edward <kaendor50@gmail.com>
2023-05-29 19:33:59 -0600

    arrow function y .reduce()

--- 04-Promedio_Mediana_Moda/main.js
+++ 04-Promedio_Mediana_Moda/main.js
@@ -1,42 +1,52 @@
 // Solucion con html y js
-const inputAgregarNumeros = document.querySelector('#inputAgregarNumero')
-const btnAgregarNumero = document.querySelector('#btnAgregarNumero');
-const btnPromediarNumeros = document.querySelector('#btnPromediarNumeros');
-const listaNumeros = document.querySelector('.listaNumeros')
-const promedioNumeros = document.querySelector('.promedioNumeros')
+// const inputAgregarNumeros = document.querySelector('#inputAgregarNumero')
+// const btnAgregarNumero = document.querySelector('#btnAgregarNumero');
+// const btnPromediarNumeros = document.querySelector('#btnPromediarNumeros');
+// const listaNumeros = document.querySelector('.listaNumeros')
+// const promedioNumeros = document.querySelector('.promedioNumeros')
 
-let arrayNumeros = [];
+// let arrayNumeros = [];
 
-btnAgregarNumero.addEventListener('click', agregarNumero);
-btnPromediarNumeros.addEventListener('click', promediarNumeros);
+// btnAgregarNumero.addEventListener('click', agregarNumero);
+// btnPromediarNumeros.addEventListener('click', promediarNumeros);
 
-function agregarNumero(){
+// function agregarNumero(){
   
-  if(inputAgregarNumeros.value){
-    arrayNumeros.push(inputAgregarNumeros.value);
-    inputAgregarNumeros.value = '';
-    listaNumeros.textContent = arrayNumeros.join('-');
+//   if(inputAgregarNumeros.value){
+//     arrayNumeros.push(inputAgregarNumeros.value);
+//     inputAgregarNumeros.value = '';
+//     listaNumeros.textContent = arrayNumeros.join('-');
 
-    if(promedioNumeros.textContent !== ''){
-      promedioNumeros.textContent = '';
-    }
-  }
-}
+//     if(promedioNumeros.textContent !== ''){
+//       promedioNumeros.textContent = '';
+//     }
+//   }
+// }
 
-function promediarNumeros(){
-  if(arrayNumeros.length >= 2){
-    let sumaNumeros = 0;
-  for(numero of arrayNumeros){
-    sumaNumeros += Number(numero);
-  }
-  const promedio = sumaNumeros/arrayNumeros.length;
+// function promediarNumeros(){
+//   if(arrayNumeros.length >= 2){
+//     let sumaNumeros = 0;
+//   for(numero of arrayNumeros){
+//     sumaNumeros += Number(numero);
+//   }
+//   const promedio = sumaNumeros/arrayNumeros.length;
   
-  promedioNumeros.textContent = promedio;
-  listaNumeros.textContent = '';
-  arrayNumeros = [];
-  }
+//   promedioNumeros.textContent = promedio;
+//   listaNumeros.textContent = '';
+//   arrayNumeros = [];
+//   }
+// }
+
+////Solucion Video con .reduce()
+function calcularPromedio(lista){
+
+//   function sumarTodosLosElementos(valorAcumulado, nuevoValor){
+//     return valorAcumulado += nuevoValor;
+//   }
+
+const sumarTodosLosElementos = (a, b)=> a += b;
+
+  const sumalista = lista.reduce(sumarTodosLosElementos)
+  return sumalista;
 }
-
-
-
 
