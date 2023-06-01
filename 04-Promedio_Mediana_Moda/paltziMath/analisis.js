3181a9235868ff93216be6822f46f6f6da5d8e4c
Harold Edward <kaendor50@gmail.com>
2023-06-01 14:34:43 -0600

    Analisis Por Empresa - Mediana de salario por año con funcion

--- 04-Promedio_Mediana_Moda/paltziMath/analisis.js
+++ 04-Promedio_Mediana_Moda/paltziMath/analisis.js
@@ -1,4 +1,4 @@
-console.log(salarios);
+console.log(salarios)
 
 
 // Analisis personal para juanita
@@ -33,4 +33,45 @@ function proyeccionPersona(nombrePersona){
     const aumentoSalario = (ultimoSalario * medianaSalario);
     const nuevoSalario = aumentoSalario + ultimoSalario;
     return nuevoSalario
+}
+// function proyeccionGastoEmpresa(nombreEmpresa, year){
+//   const arregloHistorial = [];
+//   const arraySalariosEmpresa = [];
+//   const arregloEmpresas = salarios.map(personas => personas.trabajos);
+
+//   arregloEmpresas.forEach(historial => {
+//     historial.forEach(element => arregloHistorial.push(element))
+//   });
+//   const arregloHistorialEmpresa = arregloHistorial.filter(historial => historial.empresa === nombreEmpresa && historial.year === year);
+//   arregloHistorialEmpresa.forEach(objetoEmpresa => {
+//     const salarioEmpresa = objetoEmpresa.salario;
+//     arraySalariosEmpresa.push(salarioEmpresa);
+//   });
+//   const medianaSalario = PlatziMath.calcularMediana(arraySalariosEmpresa)
+//   const promedioSalario = PlatziMath.calcularPromedio(arraySalariosEmpresa)
+//   return [medianaSalario, promedioSalario]
+// }
+
+const empresas = {};
+for(persona of salarios){
+  for(trabajo of persona.trabajos){
+    if(!empresas[trabajo.empresa]){
+      empresas[trabajo.empresa] = {};
+    }
+    if(!empresas[trabajo.empresa][trabajo.year]){
+      empresas[trabajo.empresa][trabajo.year] = [];
+    }
+
+    empresas[trabajo.empresa][trabajo.year].push(trabajo.salario)
   }
+}
+
+console.log(empresas.Freelance)
+
+function medianaEmpresa(nombreEmpresa, year){
+  if(empresas[nombreEmpresa][year]){
+    const arraySalarios = empresas[nombreEmpresa][year];
+    const medianaSalarios = PlatziMath.calcularMediana(arraySalarios);
+    return medianaSalarios
+  }
+}
